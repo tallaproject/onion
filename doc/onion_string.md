bf23b93d4d910601b8a710c5dc5c2f6c309d4d32
Alexander Færøy <ahf@0x90.dk>
2016-03-08 23:06:02 +0100

    Add onion_string:valid/2 for validating a string against an alphabet.

--- doc/onion_string.md
+++ doc/onion_string.md
@@ -14,7 +14,7 @@ __Authors:__ Alexander Færøy ([`ahf@0x90.dk`](mailto:ahf@0x90.dk)).
 ## Function Index ##
 
 
-<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#format-2">format/2</a></td><td></td></tr></table>
+<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#format-2">format/2</a></td><td></td></tr><tr><td valign="top"><a href="#valid-2">valid/2</a></td><td>Check a given string if it validates against a given alphabet.</td></tr></table>
 
 
 <a name="functions"></a>
@@ -30,4 +30,19 @@ format(Format, Data) -&gt; string()
 </code></pre>
 
 <ul class="definitions"><li><code>Format = string()</code></li><li><code>Data = [term()]</code></li></ul>
+
+<a name="valid-2"></a>
+
+### valid/2 ###
+
+<pre><code>
+valid(String, Alphabet) -&gt; boolean()
+</code></pre>
+
+<ul class="definitions"><li><code>String = string()</code></li><li><code>Alphabet = string()</code></li></ul>
+
+Check a given string if it validates against a given alphabet.
+
+This function checks a given string if all of its characters are a member
+of a given alphabet.
 
--- src/onion_string.erl
+++ src/onion_string.erl
@@ -12,7 +12,11 @@
 -module(onion_string).
 
 %% API.
--export([format/2]).
+-export([format/2,
+         valid/2
+        ]).
+
+-include("onion_test.hrl").
 
 -spec format(Format, Data) -> string()
     when
@@ -20,3 +24,42 @@
         Data   :: [term()].
 format(Format, Data) ->
     lists:flatten(io_lib:format(Format, Data)).
+
+%% @doc Check a given string if it validates against a given alphabet.
+%%
+%% This function checks a given string if all of its characters are a member
+%% of a given alphabet.
+%%
+%% @end
+-spec valid(String, Alphabet) -> boolean()
+    when
+        String   :: string(),
+        Alphabet :: string().
+valid(String, Alphabet) ->
+    Set = ordsets:from_list(Alphabet),
+    lists:all(fun (Character) ->
+                  ordsets:is_element(Character, Set)
+              end, String).
+
+-ifdef(TEST).
+
+valid_test() ->
+    [
+        ?assert(valid("", "")),
+        ?assert(valid("", "abc")),
+        ?assert(valid("a", "a")),
+        ?assert(valid("a", "aa")),
+        ?assert(valid("aa", "a")),
+        ?assert(valid("abc", "abc")),
+        ?assert(valid("abc", "abcdefg"))
+    ].
+
+valid_error_test() ->
+    [
+        ?assertNot(valid("a", "")),
+        ?assertNot(valid("ab", "")),
+        ?assertNot(valid("abc", "")),
+        ?assertNot(valid("abc", "ab"))
+    ].
+
+-endif.
