a723adf6dd58fd99677a0a90828eaed5b8894742
Alexander Færøy <ahf@0x90.dk>
2016-03-17 20:47:51 +0100

    Add test vector for x25519.

--- src/onion_x25519.erl
+++ src/onion_x25519.erl
@@ -82,3 +82,25 @@ shared_secret(SecretKey, PublicKey) ->
         Result    :: binary().
 scalarmult(SecretKey, BasePoint) ->
     enacl:curve25519_scalarmult(SecretKey, BasePoint).
+
+-ifdef(TEST).
+base16_decode(V) ->
+    {ok, Data} = onion_base16:decode(list_to_binary(V)),
+    Data.
+
+nacl_test() ->
+    AliceSecretKey = base16_decode("77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a"),
+    AlicePublicKey = base16_decode("8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a"),
+
+    BobSecretKey = base16_decode("5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb"),
+    BobPublicKey = base16_decode("de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f"),
+
+    Shared = base16_decode("4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742"),
+    [
+        ?assertEqual(AlicePublicKey, secret_key_to_public_key(AliceSecretKey)),
+        ?assertEqual(BobPublicKey, secret_key_to_public_key(BobSecretKey)),
+        ?assertEqual(Shared, shared_secret(AliceSecretKey, BobPublicKey)),
+        ?assertEqual(Shared, shared_secret(BobSecretKey, AlicePublicKey))
+    ].
+
+-endif.
