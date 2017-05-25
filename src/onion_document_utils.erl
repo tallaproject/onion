4cba0def800debdf5a795c9b7c08b8b14abab908
Lasse G. Andersen <lasse@etableret.dk>
2017-05-25 15:45:53 +0200

    Added functionality for decoding & verifying server descriptor documents.

---
+++ src/onion_document_utils.erl
@@ -0,0 +1,205 @@
+-module(onion_document_utils).
+
+-export([parse_portlist/1,
+         decode_items/2,
+         decode_bool/1,
+         decode_address/1,
+         verify_existence_properties/2,
+         verify_order/2,
+         parse_datetime/1,
+         sp_split/1,
+         binaries2integers/1
+        ]).
+
+%% @private
+decode_items(Items, Parser) ->
+    decode_items(Items, [], 1, #{}, Parser).
+
+%% @private
+-spec decode_items(ItemsIn, ItemsOut, Position, ItemOrdering, Parser) -> [ParsedItems]
+    when
+      Parser       :: function(),
+      ItemsIn      :: [term()],
+      ItemsOut     :: [term()],
+      Position     :: non_neg_integer(),
+      ItemOrdering :: #{},
+      ParsedItems  :: term().
+decode_items([{Keyword, Arguments, Object} | Rest], ItemsOut, Position, ItemOrdering, Parser) ->
+    ParsedItemData = Parser(Keyword, Arguments, Object),
+    decode_items(Rest,
+                     [{Keyword, ParsedItemData} | ItemsOut],
+                     Position + 1,
+                     update_stats(Keyword, Position, ItemOrdering),
+                     Parser);
+
+decode_items([], ItemsOut, Position, ItemOrdering, _Parser) ->
+    DocumentLength = Position - 1,
+    {lists:reverse(ItemsOut), DocumentLength, ItemOrdering}.
+
+%% @private
+-spec update_stats(Keyword, Position, DocumentStats) -> #{}
+    when
+      Keyword       :: atom() | binary(),
+      Position      :: non_neg_integer(),
+      DocumentStats :: #{}.
+update_stats(Keyword, Position, DocumentStats) ->
+    Occurences =  maps:get(Keyword, DocumentStats, []),
+    DocumentStats#{ Keyword => [Position | Occurences] }.
+
+verify_existence_properties(ItemSpecs, ItemOrdering) ->
+    true = lists:all(
+                fun ({Keyword, at_most_once}) ->
+                        at_most_once(Keyword, ItemOrdering);
+
+                    ({Keyword, exactly_once}) ->
+                        exactly_once(Keyword, ItemOrdering);
+
+                    ({Keyword, at_least_once}) ->
+                        at_least_once(Keyword, ItemOrdering);
+
+                    %% "Any Number" is only included for completeness
+                    ({_Keyword, any_number}) ->
+                        true
+
+                end, ItemSpecs),
+    ok.
+
+%% @private
+-spec at_most_once(Keyword, DocumentStats) -> true | false
+    when
+        Keyword       :: atom(),
+        DocumentStats :: #{}.
+at_most_once(Keyword, DocumentStats) ->
+    occurences_in_document(Keyword, DocumentStats) =< 1.
+
+%% @private
+-spec exactly_once(Keyword, DocumentStats) -> true | false
+    when
+      Keyword       :: atom(),
+      DocumentStats :: #{}.
+exactly_once(Keyword, DocumentStats) ->
+    occurences_in_document(Keyword, DocumentStats) =:= 1.
+
+%% @private
+-spec at_least_once(Keyword, DocumentStats) -> true | false
+    when
+      Keyword       :: atom(),
+      DocumentStats :: #{}.
+at_least_once(Keyword, DocumentStats) ->
+    occurences_in_document(Keyword, DocumentStats) >= 1.
+
+%% @private
+-spec occurences_in_document(Keyword, DocumentStats) -> non_neg_integer()
+    when
+      Keyword       :: binary() | atom(),
+      DocumentStats :: #{}.
+occurences_in_document(Keyword, DocumentStats) ->
+    length(maps:get(Keyword, DocumentStats, [])).
+
+
+%% FIXME this needs some unit-testing!
+verify_order(OrderSpec, ItemOrdering) ->
+    order_verifier(OrderSpec, ItemOrdering, 0, 0).
+
+order_verifier([{SpecKeyword, _} | RestSpec], ItemOrdering, PreviousMinPosition, PreviousMaxPosition) ->
+    %% We insert default values that will always produce {true, true} in case the SpecKeyword is not found
+    %% Checks for mandatory fields etc. is done elsewhere.
+    MinPosition = min_list(maps:get(SpecKeyword, ItemOrdering, [PreviousMinPosition])),
+    MaxPosition = max_list(maps:get(SpecKeyword, ItemOrdering, [PreviousMaxPosition])),
+    case {PreviousMinPosition =< MinPosition, PreviousMaxPosition =< MaxPosition} of
+        {true, true} ->
+            %% The next item in the spec is found and it has the correct order.
+            %% Proceed to the next item and look for the next item in spec.
+            order_verifier(RestSpec, ItemOrdering, MinPosition, MaxPosition);
+        _ ->
+            order_violated
+    end;
+
+order_verifier([], _ItemOrdering, _PreviousMinPosition, _PreviousMaxPosition) ->
+    %% We have parsed all items without any violation
+    ok.
+
+max_list([A, B | Rest]) ->
+    max_list([max(A, B) | Rest]);
+
+max_list([A]) ->
+    A.
+
+min_list([A, B | Rest]) ->
+    min_list([min(A, B) | Rest]);
+
+min_list([A]) ->
+    A.
+
+%% @private
+-spec parse_datetime(DateTimeRaw) -> DateTime
+    when
+        DateTimeRaw :: binary(),
+        DateTime    :: calendar:datetime().
+parse_datetime(<<Year:4/binary, "-", Month:2/binary, "-", Day:2/binary, " ", Hour:2/binary, ":", Minutes:2/binary, ":", Seconds:2/binary>>) ->
+    Date = erlang:list_to_tuple(binaries2integers([Year, Month, Day])),
+    Time = erlang:list_to_tuple(binaries2integers([Hour, Minutes, Seconds])),
+    {Date, Time}.
+
+%% @private
+-spec parse_portlist(PortListEncoded) -> [PortOrRange]
+    when
+      PortListEncoded :: binary(),
+      PortOrRange     :: inet:port_number() | {inet:port_number(), inet:port_number()}.
+parse_portlist(PortListEncoded) ->
+    lists:map(fun decode_port_or_range/1, binary:split(PortListEncoded, <<",">>, [global])).
+
+%% @private
+-spec decode_port_or_range(PortOrRangeEncoded) -> [PortOrRange]
+    when
+      PortOrRangeEncoded :: binary(),
+      PortOrRange        :: inet:port_number() | {inet:port_number(), inet:port_number()}.
+decode_port_or_range(PortOrRange) ->
+    case binary:split(PortOrRange, <<"-">>) of
+        [PortStart, PortEnd] ->
+            binaries2integers([PortStart, PortEnd]);
+        [Port] ->
+            erlang:binary_to_integer(Port)
+    end.
+
+%% @private
+-spec decode_bool(binary()) -> boolean().
+decode_bool(<<"1">>) ->
+    true;
+
+decode_bool(<<"0">>) ->
+    false.
+
+%% @private
+-spec decode_address(AddressRaw) -> {inet:ip_address(), inet:port()}
+    when
+        AddressRaw :: binary().
+decode_address(<<"[", IPv6Raw/binary>>) ->
+    [IPv6_, Port] = binary:split(IPv6Raw, <<"]:">>),
+    %% This would also pass on an IPv4 address
+    {ok, IPv6} = inet:decode_address(erlang:binary_to_list(IPv6_)),
+    {IPv6, erlang:binary_to_integer(Port)};
+
+decode_address(IPv4Raw) ->
+    [IPv4_, Port] = binary:split(IPv4Raw, <<":">>),
+    {ok, IPv4} = inet:decode_address(erlang:binary_to_list(IPv4_)),
+    {IPv4, erlang:binary_to_integer(Port)}.
+
+
+
+%% @private
+-spec binaries2integers([binary()]) -> [integer()].
+binaries2integers(Binaries) ->
+    lists:map(fun erlang:binary_to_integer/1, Binaries).
+
+
+%% @private
+-spec  sp_split(Arguments) -> [Argument]
+    when
+      Arguments :: binary() | no_arguments,
+      Argument  :: binary().
+sp_split(no_arguments) ->
+    [];
+
+sp_split(Binary) ->
+    binary:split(Binary, <<" ">>, [global]).
--- src/onion_server_descriptor.erl
+++ src/onion_server_descriptor.erl
@@ -16,7 +16,8 @@
 -module(onion_server_descriptor).
 
 %% API.
--export([encode/1]).
+-export([encode/1,
+         decode/1]).
 
 -include("onion_test.hrl").
 
@@ -116,3 +117,431 @@ encode(ServerDescriptor) ->
         %% "router-signature" NL Signature (the Signature is added outside of this function).
         {'router-signature', []}
     ]).
+
+
+%% @doc Parse and verify a server descriptor document.
+-spec decode(Document) -> {ok, ParsedItems}
+    when
+      Document    :: binary(),
+      ParsedItems :: [term()].
+decode(Document) ->
+    ServerDescriptorSpec = [
+                            {router,                     exactly_once},
+                            {'identity-ed25519',         at_most_once},
+                            {'master-key-ed25519',       at_most_once},
+                            {bandwidth,                  exactly_once},
+                            {platform,                   at_most_once},
+                            {published,                  exactly_once},
+                            {fingerprint,                at_most_once},
+                            {hibernating,                at_most_once},
+                            {uptime,                     at_most_once},
+                            {'onion-key',                exactly_once},
+                            {'onion-key-crosscert',      at_most_once},
+                            {'ntor-onion-key',           at_most_once},
+                            {'ntor-onion-key-crosscert', at_most_once},
+                            {'signing-key',              exactly_once},
+                            {accept,                     any_number  },
+                            {reject,                     any_number  },
+                            {'ipv6-policy',              at_most_once},
+                            {'router-sig-ed25519',       at_most_once},
+                            {'router-signature',         exactly_once},
+                            {contact,                    at_most_once},
+                            {family,                     at_most_once},
+                            {'read-history',             at_most_once},
+                            {'write-history',            at_most_once},
+                            {eventdns,                   at_most_once},
+                            {'caches-extra-info',        at_most_once},
+                            {'extra-info-digest',        at_most_once},
+                            {'hidden-service-dir',       at_most_once},
+                            {protocols,                  at_most_once},
+                            {'allow-single-hop-exits',   at_most_once},
+                            {'or-address',               any_number  },
+                            {'tunnelled-dir-server',     at_most_once},
+                            {proto,                      at_most_once}
+                           ],
+
+    %% Basic parsing, decoding & verification
+    {ok, Items} = onion_document:decode(Document),
+    {ParsedItems, DocumentLength, ItemOrder} = onion_document_utils:decode_items(Items, fun item_decoder/3),
+    ok = onion_document_utils:verify_existence_properties(ServerDescriptorSpec, ItemOrder),
+
+    %% Router entry should exist and be the first
+    #{router := [1]} = ItemOrder,
+
+    %% identity-ed25519 should be second if it's present
+    [2] = maps:get('identity-ed25519', ItemOrder, [2]),
+
+    %% Verify existence & position of document signatures
+    SecondLast = DocumentLength - 1,
+    [SecondLast] = maps:get('router-sig-ed25519', ItemOrder, [SecondLast]),
+    [DocumentLength] = maps:get('router-signature', ItemOrder),
+
+    %% [At most once, required when identity-ed25519 is present]
+    %% The spec actually says actually says "forbidden otherwise" for
+    %% onion-key-crosscert but we do not check that. Not sure it is necessary.
+    required_if_identity_ed25519_is_present(['ntor-onion-key-crosscert',
+                                             'router-sig-ed25519',
+                                             'onion-key-crosscert'], ItemOrder),
+
+    %% Certificate verification
+    DocumentBin = erlang:list_to_binary(Document),
+    ok = verify_identity_ed25519_cert(ParsedItems),
+    ok = verify_onion_key_crosscert(ParsedItems),
+    ok = verify_ntor_onion_key_crosscert(ParsedItems),
+    ok = verify_rsa_signature(DocumentBin, ParsedItems),
+    ok = verify_ed25519_router_signature(DocumentBin, ParsedItems),
+    {ok, lists:reverse(ParsedItems)}.
+
+
+%% @private
+-spec verify_rsa_signature(Document, Items) -> ok
+    when
+        Document :: binary(),
+        Items    :: [term()].
+verify_rsa_signature(Document, Items) ->
+    %% Extract public key & signature
+    SigningKeyDER = proplists:get_value('signing-key', Items),
+    {ok, SigningKey} = onion_rsa:der_decode_public_key(SigningKeyDER),
+    Signature = proplists:get_value('router-signature', Items),
+
+    %% Create message digest
+    {Offset, Length} = binary:match(Document, <<"\nrouter-signature\n">>),
+    Message = binary:part(Document, {0, Offset + Length}),
+    MessageDigest = crypto:hash(sha, Message),
+
+    %% Verify that signature is encrypted message digest
+    MessageDigest = onion_rsa:public_decrypt(Signature, SigningKey, rsa_pkcs1_padding),
+    ok.
+
+
+%% @private
+-spec verify_ed25519_router_signature(Document, Items) -> ok
+    when
+        Document :: binary(),
+        Items    :: [term()].
+verify_ed25519_router_signature(Document, Items) ->
+    case proplists:get_value('identity-ed25519', Items) of
+        {#{ cert_key := SignedKey}, _, _} ->
+            %% Construct signed message
+            {Offset, Length} = binary:match(Document, <<"\nrouter-sig-ed25519 ">>),
+            MessagePart = binary:part(Document, {0, Offset + Length}),
+            Message = <<"Tor router descriptor signature v1", MessagePart/binary>>,
+            MessageDigest = crypto:hash(sha256, Message),
+
+            %% Verify message signature
+            Signature = proplists:get_value('router-sig-ed25519', Items),
+            true = onion_ed25519:open(Signature, MessageDigest, SignedKey),
+            ok;
+
+        %% we check if this item and identity-ed25519 co-exist elsewhere
+        undefined ->
+            ok
+    end.
+
+
+%% @private
+-spec required_if_identity_ed25519_is_present(ItemsRequired, DocumentStats) -> ok
+    when
+      ItemsRequired :: [atom()],
+      DocumentStats :: #{}.
+required_if_identity_ed25519_is_present(ItemsRequired, DocumentStats) ->
+    case maps:get('identity-ed25519', DocumentStats, not_found) of
+        not_found ->
+            ok;
+        _ ->
+            true = lists:all(fun (Keyword) -> maps:is_key(Keyword, DocumentStats) end, ItemsRequired),
+            ok
+    end.
+
+
+%% @private
+-spec item_decoder(Keyword, Arguments, Object) -> [ParsedItem]
+  when
+    Keyword    :: atom() | binary(),
+    Arguments  :: binary(),
+    Object     :: binary(),
+    ParsedItem :: term().
+item_decoder(published, UTCTime, no_object) ->
+    <<Year:4/binary, "-", Month:2/binary, "-", Day:2/binary, " ", Hour:2/binary, ":", Minutes:2/binary, ":", Seconds:2/binary>> = UTCTime,
+    {Year, Month, Day, Hour, Minutes, Seconds};
+
+item_decoder(fingerprint, FingerprintRaw, no_object) ->
+    onion_binary:fingerprint(FingerprintRaw);
+
+item_decoder(hibernating, BoolRaw, no_object) ->
+    onion_document_utils:decode_bool(BoolRaw);
+
+item_decoder(uptime, Uptime, no_object) ->
+    erlang:binary_to_integer(Uptime);
+
+item_decoder(family, NamesBinary, no_object) ->
+    Names = [binary_to_list(NameBin) || NameBin <- onion_document_utils:sp_split(NamesBinary)],
+    lists:map(fun (Name) ->
+                  true = onion_relay:valid_nickname_or_relay_fingerprint(Name)
+              end, Names),
+    Names;
+
+item_decoder(eventdns, BoolRaw, no_object) ->
+    %% not seen in any cached server-descriptors
+    onion_document_utils:decode_bool(BoolRaw);
+
+item_decoder(router, Arguments, no_object) ->
+    %% Ignore any remaining arguments according to dir-spec section 2.1.1
+    [NickName, Address, ORPortBin, SOCKSPortBin, DirPortBin | _RestArguments] = onion_document_utils:sp_split(Arguments),
+    [ORPort, SOCKSPort, DirPort] = onion_document_utils:binaries2integers([ORPortBin, SOCKSPortBin, DirPortBin]),
+    0 = SOCKSPort, % According to spec
+    #{ nickname => NickName,
+       address  => inet:parse_ipv4_address(binary_to_list(Address)),
+       or_port  => ORPort,
+       dir_port => DirPort};
+
+item_decoder(bandwidth, Arguments, no_object) ->
+    ArgumentList = binary:split(Arguments, <<" ">>, [global]),
+    [BandwidthAvg, BandwidthBurst, BandwidthObserved] = onion_document_utils:binaries2integers(ArgumentList),
+    #{ bandwidth_average  => BandwidthAvg,
+       bandwidth_burst    => BandwidthBurst,
+       bandwidth_observed => BandwidthObserved };
+
+item_decoder('ipv6-policy', PolicyRaw, no_object) ->
+    %% FIXME (lga) A missing "ipv6-policy" line is equivalent to "ipv6-policy reject 1-65535"
+    %% According to dir-spec.txt L2122-2130:
+    %% FIXME (lga) verify that portnumbers are valid integers
+    case PolicyRaw of
+        <<"accept ", PortList/binary>> ->
+            {accept, onion_document_utils:parse_portlist(PortList)};
+        <<"reject ", PortList/binary>> ->
+            {reject, onion_document_utils:parse_portlist(PortList)}
+    end;
+
+item_decoder(proto, EntriesRaw, no_object) ->
+    Entries = onion_document_utils:sp_split(EntriesRaw),
+    lists:map(fun parse_proto_entry/1, Entries);
+
+item_decoder('or-address', Address, no_object) ->
+    onion_document_utils:decode_address(Address);
+
+item_decoder('extra-info-digest', Digests, no_object) ->
+    case DigestList = onion_document_utils:sp_split(Digests) of
+        [Sha1Digest] ->
+            true = onion_base16:valid(Sha1Digest);
+        [Sha1Digest, Sha256Digest] ->
+            true = onion_base16:valid(Sha1Digest),
+            true = onion_base64:valid(Sha256Digest)
+    end,
+    DigestList;
+item_decoder('hidden-service-dir', VersionNumsBin, no_object) ->
+    case onion_document_utils:binaries2integers(onion_document_utils:sp_split(VersionNumsBin)) of
+        [] ->
+            [2];
+        MultipleVersions ->
+            % FIXME (lga) do a check on version numbers to check they are valid
+            MultipleVersions
+     end;
+
+%% tor-dir.txt section 2.1.2.1.:
+%%
+%% "Tools should continue to accept read-history and write-history values
+%% in server descriptors produced by older versions of Tor
+%% until all Tor versions earlier than 0.2.0.x are obsolete."
+%%
+%% Do we need to parse them at all?
+item_decoder('write-history', History, no_object) ->
+    History;
+item_decoder('read-history', History, no_object) ->
+    History;
+
+%% FIXME (lga) reject / accept items needs some subtle parsing, i think.
+%% Should probably write a lexer/parser for this. See section 2.1.3. on details
+item_decoder(accept, ExitPattern, no_object) ->
+    ExitPattern;
+
+item_decoder(reject, ExitPattern, no_object) ->
+    ExitPattern;
+
+item_decoder(protocols, <<"protocols Link 1 2 Circuit 1">>, no_object) ->
+    %% dir-spec L690-691:
+    %% As of 30 Mar 2008, specified protocols are "Link 1 2 Circuit 1".
+    #{ link => [1,2], circuit => 1 };
+
+item_decoder('caches-extra-info', Arguments, Object) ->
+    true = no_extra_arguments({Arguments, Object});
+
+item_decoder('allow-single-hop-exits', Arguments, Object) ->
+    true = no_extra_arguments({Arguments, Object});
+
+item_decoder('tunnelled-dir-server', Arguments, Object) ->
+    true = no_extra_arguments({Arguments, Object});
+
+item_decoder(platform, Platform, no_object) ->
+    Platform;
+
+item_decoder(contact, ContactInfo, no_object) ->
+    ContactInfo;
+
+item_decoder('identity-ed25519', Arguments, {<<"ED25519 CERT">>, CertRaw}) ->
+    true = no_extra_arguments(Arguments),
+    {ok, Cert} = onion_base64:decode(CertRaw),
+    onion_cert:decode(Cert);
+
+item_decoder('master-key-ed25519', MasterKeyBase64, no_object) ->
+    {ok, MasterKey} = onion_base64:decode(MasterKeyBase64),
+    MasterKey;
+
+item_decoder('onion-key', Arguments, {<<"RSA PUBLIC KEY">>, RSAKeyRaw}) ->
+    true = no_extra_arguments(Arguments),
+    decode_and_verify_onion_key(RSAKeyRaw);
+
+item_decoder('onion-key-crosscert', Arguments, {<<"CROSSCERT">>, CertRaw}) ->
+    true = no_extra_arguments(Arguments),
+    {ok, Cert} = onion_base64:decode(CertRaw),
+    Cert;
+
+item_decoder('ntor-onion-key', PublicKeyBase64, no_object) ->
+    {ok, PublicKey} = onion_base64:decode(PublicKeyBase64),
+    PublicKey;
+
+item_decoder('ntor-onion-key-crosscert', BitRaw, {<<"ED25519 CERT">>, CertRaw}) ->
+    %% This item also has a [No extra arguments] clause, but these two checks ensure that
+    {ok, CertEncoded} = onion_base64:decode(CertRaw),
+    {Cert, Message, Signature} = onion_cert:decode(CertEncoded),
+    Bit = erlang:binary_to_integer(BitRaw),
+    {Bit, Cert, Message, Signature};
+
+item_decoder('signing-key', Arguments, {<<"RSA PUBLIC KEY">>, PublicKeyBase64}) ->
+    true = no_extra_arguments(Arguments),
+    {ok, PublicKey} = onion_base64:decode(PublicKeyBase64),
+    PublicKey;
+
+item_decoder('router-sig-ed25519', SignatureBase64, no_object) ->
+    %% This item does not have a [No extra arguments] in spec.. should it?
+    {ok, Signature} = onion_base64:decode(SignatureBase64),
+    Signature;
+
+item_decoder('router-signature', Arguments, {<<"SIGNATURE">>, SignatureBase64}) ->
+    true = no_extra_arguments(Arguments),
+    {ok, Signature} = onion_base64:decode(SignatureBase64),
+    Signature;
+
+
+item_decoder(_Keyword, Arguments, Object) ->
+    %% We ignore unknown items, according to dir-spec (section 1.2 L222-225)
+    {Arguments, Object}.
+
+
+%% @private
+-spec no_extra_arguments(ExtraInput) -> boolean()
+    when
+      ExtraInput :: term().
+no_extra_arguments(no_arguments) ->
+    true;
+no_extra_arguments({no_arguments, no_object}) ->
+    true;
+no_extra_arguments(_) ->
+    false.
+
+
+%% @private
+-spec parse_proto_entry(Entry) -> {atom(), [binary()]}
+    when
+      Entry :: binary().
+parse_proto_entry(Entry) ->
+    %% This could be more precise. Fix when needed.
+    [KeyWord, Values] = binary:split(Entry, <<"=">>),
+    ValuesList = binary:split(Values, <<",">>),
+    {erlang:binary_to_atom(KeyWord, utf8), ValuesList}.
+
+
+%% @private
+-spec verify_identity_ed25519_cert(Items) -> ok
+    when
+        Items :: [term()].
+verify_identity_ed25519_cert(Items) ->
+    %% In the spec the following prefix should be used in the message of the cert
+    %%  <<"Tor node signing key certificate v1">>
+    %% However, it seems that this is not used by tor.
+    {Cert, Message, Signature} = proplists:get_value('identity-ed25519', Items),
+
+    %% Verify certificate type and extract Ed25519Key
+    #{ version := 1,
+       cert_type := 4,
+       cert_key_type := 1,
+       extensions := [{ExtensionType, ExtensionFlags, Ed25519Key}] } = Cert,
+    4 = ExtensionType,
+    0 = ExtensionFlags,
+
+    %% Verify that if the 'master-key-ed25519' item exists it should be equal the key used here
+    Ed25519Key = proplists:get_value('master-key-ed25519', Items, Ed25519Key),
+
+    %% Verify certificate signature
+    true = onion_ed25519:open(Signature, Message, Ed25519Key),
+    ok.
+
+
+%% @private
+-spec verify_onion_key_crosscert(Items) -> ok
+    when
+        Items :: [term()].
+verify_onion_key_crosscert(Items) ->
+    %% Extract keys and signature
+    Signature = proplists:get_value('onion-key-crosscert', Items),
+    OnionPubkey = proplists:get_value('onion-key', Items),
+    IdentityKey = get_ed25519_identity_key(Items),
+    SigningKey = proplists:get_value('signing-key', Items),
+
+    %% Verify signature
+    SigningKeyDigest = crypto:hash(sha, SigningKey),
+    DecryptedData = onion_rsa:public_decrypt(Signature, OnionPubkey, rsa_no_padding),
+    Payload = <<SigningKeyDigest/binary, IdentityKey/binary>>,
+    CommonSuffix = binary:longest_common_suffix([Payload, DecryptedData]),
+    CommonSuffix = size(Payload),
+    ok.
+
+
+%% @private
+-spec verify_ntor_onion_key_crosscert(Items) -> ok
+    when
+        Items :: [term()].
+verify_ntor_onion_key_crosscert(Items) ->
+    %% Extract certificate & signing key
+    PubkeyCurve25519 = proplists:get_value('ntor-onion-key', Items),
+    {Bit, CrossCert, Message, Signature} = proplists:get_value('ntor-onion-key-crosscert', Items),
+    Ed25519Key = onion_ed25519:public_key_from_x25519_public_key(PubkeyCurve25519, Bit),
+
+    %% Verify certificate type and that key from identity-ed25519 is equal to cert_key
+    #{ version       := 1,
+       cert_type     := 10, % 1a in hex used in dir-spec
+       cert_key      := IdentityKey,
+       cert_key_type := 1,
+       extensions    := [] } = CrossCert,
+
+    %% Verify that the 'identity-ed25519' is the same as the signed key in crosscert
+    IdentityKey = get_ed25519_identity_key(Items),
+
+    %% Verify certificate
+    true = onion_ed25519:open(Signature, Message, Ed25519Key),
+    ok.
+
+
+%% @private
+-spec decode_and_verify_onion_key(RSAKeyBase64) -> RSAKey
+    when
+        RSAKeyBase64 :: binary(),
+        RSAKey       :: onion_rsa:public_key().
+decode_and_verify_onion_key(RSAKeyBase64) ->
+    {ok, RSAKeyDER} = onion_base64:decode(RSAKeyBase64),
+    {ok, RSAKey} = onion_rsa:der_decode_public_key(RSAKeyDER),
+    true = (1024 =:= onion_rsa:key_size(RSAKey)),
+    RSAKey.
+
+%% @private
+-spec get_ed25519_identity_key(Items) -> IdentityKey | no_key
+    when
+        Items       :: [term()],
+        IdentityKey :: binary().
+get_ed25519_identity_key(Items) ->
+    case proplists:get_value('identity-ed25519', Items) of
+        {#{ extensions := [{_, _, IdentityKey}] }, _, _} ->
+            IdentityKey;
+        _ ->
+            no_key
+    end.
